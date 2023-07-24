47a913bafa980a6c8deedc8b19ef7393855dc92f
Leonardo Hernandez <leonardo.gomes@venicetech.com.br>
2023-07-24 08:53:33 -0300

    log-changes

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -137,11 +137,11 @@ Cypress.Commands.add("loginByXstate", (username, password = Cypress.env("default
 
   cy.intercept("POST", "/login").as("loginUser");
   cy.intercept("GET", "/checkAuth").as("getUserProfile");
-  cy.visit("/signin", { log: false }).then(() => {
+  cy.visit("/signin", { log: true }).then(() => {
     log.snapshot("before");
   });
 
-  cy.window({ log: false }).then((win) => win.authService.send("LOGIN", { username, password }));
+  cy.window({ log: true }).then((win) => win.authService.send("LOGIN", { username, password }));
 
   cy.wait("@loginUser").then((loginUser) => {
     log.set({
